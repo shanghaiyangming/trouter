0d160f34b3a605df41b7f34f3f65a2d904cd1169
Young <shanghaiyangming@gmail.com>
2015-03-03 11:14:45 +0800

    暂时关闭redis存储功能，待开发

--- conf/redis_conn.py
+++ conf/redis_conn.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 import redis
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
-
+#redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_client = ""
